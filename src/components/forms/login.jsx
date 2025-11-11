ccaa4957ec9395c0005de037e9d455eec2fc2f72
11yashiagrawal <11yashiagrawal@gmail.com>
2025-11-12 03:05:50 +0530

    temp

--- src/components/forms/login.jsx
+++ src/components/forms/login.jsx
@@ -1,4 +1,5 @@
-// "use client";
-
-// import { useState } from "react";
-// import { TextField, Button, Typography } from "@mui/material";
+export default function Login(){
+    return (
+        <></>
+    )
+}
